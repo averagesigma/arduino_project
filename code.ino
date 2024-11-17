c446ae201596ff74880dbbda73851d71335dc910
averagesigma <shkolnik.yoni@gmail.com>
2024-11-17 16:35:14 +0200

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,26 +1,31 @@
 void setup() {
   // put your setup code here, to run once:
 
+
+yoni_blink(8, 300, 4);
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  yoni_blink(8, 300);
 }
 
-void yoni_blink(int pin, int count) {
+void yoni_blink(int pin, int count, int count_flash) {
   // initialize the LED as an output
-  pinMode(pin, OUTPUT);
+  for (int i = 0; i < count_flash; i++){
+    pinMode(pin, OUTPUT);
 
-  // tunr on the LED
-digitalWrite(pin, HIGH);
+    // tunr on the LED
+    digitalWrite(pin, HIGH);
 
-// wait for a set amount of milliseconds
-delay(count);
+    // wait for a set amount of milliseconds
+    delay(count);
 
-// turn the LED off 
-digitalWrite(pin, LOW);
+    // turn the LED off 
+    digitalWrite(pin, LOW);
 
-// wait for a set amount of milliseconds
-delay(count);
+    // wait for a set amount of milliseconds
+    delay(count);
+  }
 }
